1134f7d19ae42c49325176c06c3b6399c31f1809
SusanGithaigaN <susan.githaiga@student.moringaschool.com>
2023-03-25 03:42:44 +0300

    Completed assignment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,35 @@
 class UsersController < ApplicationController
+    before_action :authorize, only: [:show]
+
+    def create 
+        user = User.create(user_details)
+        if user.valid?
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: { errors: user.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+    
+    def show
+        user = User.find(session[:user_id])
+        # if user
+            render json: user
+        # else
+            # render json: { error: "Not authorized" }, status: :unauthorized
+        # end
+    end
+
+
+    private
+
+    def user_details
+        params.permit( :username, :password, :password_confirmation )
+    end
+
+    def authorize
+        render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
+            
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  resources :users
+  # resources :users
+  post '/signup', to: 'users#create'
+  # user login
+  post '/login', to: 'sessions#create'
+  # logout
+  delete '/logout', to: 'sessions#destroy'
+  get '/me', to: 'users#show'
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
